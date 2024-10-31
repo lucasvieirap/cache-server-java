50b41a8144bcc8ee0a6d3ef894b30237cbadf6a2
lucsvp <lucasvieirapalavro@gmail.com>
2024-10-31 19:25:03 -0300

    initial commit

---
+++ src/main/java/com/vieira/cache_server/CacheServerApplication.java
@@ -0,0 +1,13 @@
+package com.vieira.cache_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vieira/cache_server/CacheServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieira.cache_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
